af6bef1021215c7d4f660a70d802b21e0ab851b6
haoxinsun <haoxinsun@gmail.com>
2014-10-28 13:46:01 +0800

    divide-and-conquer
    introduction to algorithms ,chapter 4,divide-and-conquer

---
+++ ConsoleApplication1/ConsoleApplication1/Define_function.cpp
@@ -0,0 +1,65 @@
+#include"Header.h"
+
+
+//find the a maximum subarray in arry A using O(nlgn) running time
+//the idea  to handle the problem is divide and conquer
+//author:Haoxin Sun data:10-28-2014
+//member function to calculate the sum of elements between low and high iterator
+double maximum_subarray::calculate_sum()
+{
+	double add_ele = 0;
+	for (vector<int>::iterator curr = low; curr <= high; ++curr)
+	{
+		add_ele += *curr;
+	}
+	return add_ele;
+}
+maximum_subarray Find_maximum_subarray(vector<int> A, vector<int>::iterator low, vector<int>::iterator high)
+{
+	maximum_subarray Max_cross_subarray(vector<int> A, vector<int>::iterator low, vector<int>::iterator middle, vector<int>::iterator high);
+	if (low == high)
+	{
+		maximum_subarray sub(low, high);
+		return sub;
+	}
+	vector<int>::iterator middle = low + (high - low) / 2;
+	//these three objects to hold the left ,cross, right maximum subarray
+	maximum_subarray l_max;
+	maximum_subarray r_max;
+	maximum_subarray cross_max;
+	l_max = Find_maximum_subarray(A, low, middle);
+	r_max = Find_maximum_subarray(A, middle + 1, high);
+	cross_max = Max_cross_subarray(A, low, middle, high);
+	//define a class object to hold the a maximum subarray from the three objects
+	maximum_subarray max_sub;
+	max_sub = l_max.get_sum() > r_max.get_sum() ? (l_max.get_sum() > cross_max.get_sum() ? l_max : cross_max) : (r_max.get_sum() > cross_max.get_sum() ? r_max : cross_max);
+	return max_sub;
+}
+
+//calculate the maximum subarray that  cross middle point 
+maximum_subarray Max_cross_subarray(vector<int> A, vector<int>::iterator low, vector<int>::iterator middle, vector<int>::iterator high)
+{
+	//calculate the maximum subarray of left part(included middle)
+	maximum_subarray l_sub(middle, middle);
+	for (int curr = 0; curr <= middle - low; ++curr)
+	{
+		maximum_subarray curr_sum(middle - curr, middle);
+		//double cur_sum = *(middle - curr - 1) + l_sub.get_sum();
+		if (curr_sum.get_sum()>l_sub.get_sum())
+		{
+			l_sub = curr_sum;
+		}
+	}
+	// calculate the right part
+	maximum_subarray r_sub(middle + 1, middle + 1);
+	for (vector<int>::iterator r_beg = middle + 1; r_beg <= high; ++r_beg)
+	{
+		maximum_subarray curr_sum(middle + 1, r_beg);
+		if (curr_sum.get_sum() > r_sub.get_sum())
+		{
+			r_sub = curr_sum;
+		}
+	}
+	maximum_subarray cross_sub(l_sub.get_low(), r_sub.get_high());
+	return cross_sub;
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Header.h
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+//find the a maximum subarray in arry A using O(nlgn) running time
+//the idea  to handle the problem is divide and conquer
+//author:Haoxin Sun data:10-28-2014
+class maximum_subarray
+{
+public:
+	maximum_subarray(){}
+	maximum_subarray(vector<int>::iterator low, vector<int>::iterator high) :low(low), high(high), sum(calculate_sum()){}
+	vector<int>::iterator get_low(){ return low; }
+	vector<int>::iterator get_high(){ return high; }
+	double get_sum(){ return sum; }
+private:
+	double calculate_sum();
+	vector<int>::iterator low;
+	vector<int>::iterator high;
+	double sum;
+};
+maximum_subarray Find_maximum_subarray(vector<int> A, vector<int>::iterator low, vector<int>::iterator high);
+maximum_subarray Max_cross_subarray(vector<int> A, vector<int>::iterator low, vector<int>::iterator middle, vector<int>::iterator high);
